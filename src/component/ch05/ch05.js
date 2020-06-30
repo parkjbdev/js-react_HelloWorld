df6e63bf32dadad88beeb8774f0f3fad002c05f7
parkjbdev <parkjbdev@gmail.com>
2020-06-30 22:35:33 +0900

    CH05-3

--- src/component/ch05/ch05.js
+++ src/component/ch05/ch05.js
@@ -1,12 +1,50 @@
 import React from "react";
 
 class App extends React.Component {
-    state = {
-        count: 0,
+    constructor(props) {
+        super(props);
+        //console.log('hello');
+        this.state = {
+            count: 0,
+        };
     }
+    componentDidMount() {
+        console.log('component rendered');
+    }
+    componentDidUpdate(prevProps, prevState, snapshot) {
+        console.log('component updated');
+    }
+    componentDidCatch(error, errorInfo) {
+
+    }
+    componentWillUnmount() {
+        console.log('bye!');
+    }
+
+    add = () => {
+        console.log('add');
+        this.setState({
+            count: this.state.count + 1,
+        })
+    };
+
+    minus = () => {
+        console.log('minus');
+        this.setState({
+            count: this.state.count - 1,
+        })
+    };
+
+
     render() {
-        return <h1>I'm a class component</h1>;
-    }
+        return (
+            <div>
+                <h1>The number is: {this.state.count}</h1>
+                <button onClick={this.add}>Add</button>
+                <button onClick={this.minus}>Minus</button>
+            </div>
+        );
+    };
 }
 
 export default App;
