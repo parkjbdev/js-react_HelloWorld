4189b53d989010dcddfa97688cb77b87375705ff
parkjbdev <parkjbdev@gmail.com>
2020-06-30 17:58:29 +0900

    CH05 First Commit

--- src/component/ch05/ch05.js
+++ src/component/ch05/ch05.js
@@ -1,7 +1,12 @@
 import React from "react";
 
 class App extends React.Component {
+    state = {
+        count: 0,
+    }
     render() {
         return <h1>I'm a class component</h1>;
     }
 }
+
+export default App;
--- src/index.js
+++ src/index.js
@@ -1,14 +1,16 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import App from './component/DefaultApp/App';
-import Game from './component/TicTacToe/TicTacToe';
+// import App from './component/DefaultApp/App';
+// import Game from './component/TicTacToe/TicTacToe';
+import App from './component/ch05/ch05'
 import * as serviceWorker from './serviceWorker';
 
 ReactDOM.render(
   <React.StrictMode>
     {/*<App />*/}
-    <Game />
+    {/*<Game />*/}
+    <App />
   </React.StrictMode>,
   document.getElementById('root')
 );
