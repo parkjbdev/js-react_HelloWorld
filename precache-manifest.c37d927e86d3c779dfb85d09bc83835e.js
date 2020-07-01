7ad0c70c3cf5bb3a1ac3df1a56ac4a560c170f8e
parkjbdev <parkjbdev@gmail.com>
2020-07-02 04:41:15 +0900

    Updates

---
+++ precache-manifest.c37d927e86d3c779dfb85d09bc83835e.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c05bd060e9b8ec4e1cc410e64fc5174f",
+    "url": "/js-react_HelloWorld/index.html"
+  },
+  {
+    "revision": "43d94eb37323929994bd",
+    "url": "/js-react_HelloWorld/static/css/main.e4d39d66.chunk.css"
+  },
+  {
+    "revision": "d580d6a1dce06e1250c2",
+    "url": "/js-react_HelloWorld/static/js/2.08ea9292.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/js-react_HelloWorld/static/js/2.08ea9292.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "43d94eb37323929994bd",
+    "url": "/js-react_HelloWorld/static/js/main.2074cecd.chunk.js"
+  },
+  {
+    "revision": "5ef5191b61ad34c13fcd",
+    "url": "/js-react_HelloWorld/static/js/runtime-main.595933bc.js"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/js-react_HelloWorld/precache-manifest.341ed481bdb4de76ea2cd13bc6072fa1.js"
+  "/js-react_HelloWorld/precache-manifest.c37d927e86d3c779dfb85d09bc83835e.js"
 );
 
 self.addEventListener('message', (event) => {
